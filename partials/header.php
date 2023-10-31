828790981c4690f3a8bdca4c417f41034915d9ae
[enzoPOHU] <[enzopohu91@gmail.com]>
2023-10-31 13:23:00 +0100

    story3

--- partials/header.php
+++ partials/header.php
@@ -2,11 +2,19 @@
 
         <div class="titre"> <p><b>Shinkei Suijaku</b></p>
             <nav>
-                <ul>
-                   <li> <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>index.php"><b>ACCUEIL</b></a> </li>
-                   <li> <a style="text-decoration: none;"href="<?= PROJECT_FOLDER ?>games/memory/index.php"><b>JEU</b></a> </li>
-                   <li> <a style="text-decoration: none;"href="<?= PROJECT_FOLDER ?>games/memory/scores.php"><b>SCORES</b></a> </li>
-                    <li><a style="text-decoration: none;"href="<?= PROJECT_FOLDER ?>contact.php"><b>NOUS CONTACTER</b></a> </li>
+                <ul class="menu">
+
+                   <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER.'index.php') ? 'class="active"' : ''; ?>>
+                   <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>index.php"><b>ACCUEIL</b></a> </li>
+                   
+                   <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER.'games/memory/index.php') ? 'class="active"' : ''; ?>>
+                   <a style="text-decoration: none;"href="<?= PROJECT_FOLDER ?>games/memory/index.php"><b>JEU</b></a> </li>
+                   
+                   <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER.'games/memory/scores.php') ? 'class="active"' : ''; ?>>
+                   <a style="text-decoration: none;"href="<?= PROJECT_FOLDER ?>games/memory/scores.php"><b>SCORES</b></a> </li>
+                    
+                   <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER.'contact.php') ? 'class="active"' : ''; ?>>
+                   <a style="text-decoration: none;"href="<?= PROJECT_FOLDER ?>contact.php"><b>NOUS CONTACTER</b></a> </li>
                 </ul>
             </nav>
         </div>
