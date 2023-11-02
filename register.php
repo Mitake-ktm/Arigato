8ac198ff4d8f1fb78783e4a95fce0908a5f9e110
Antony Dias <antony.overwatch@gmail.com>
2023-11-02 13:14:36 +0100

    page d'incription

--- register.php
+++ register.php
@@ -14,7 +14,77 @@ require_once SITE_ROOT . 'utils/database.php';
 </div>
 
 <div class="Formulaire_de_connexion">
-    <form method="post" action="traitement.php">
+
+<?php
+    if (!empty($_GET)) {
+
+        if (isset($_GET['email'])) {
+            
+            if (!filter_var($_GET['email'], FILTER_VALIDATE_EMAIL)) 
+            {
+                $error_message_mail = "Le format de l'email n'est pas valide";
+            }
+            
+        }
+
+        if (isset($_GET['pseudo'])){
+
+            if (strlen('pseudo') < 4 ){
+
+                $error_message_pseudo = 'le pseudo doit etre plus long que 4 carctère';
+            }
+            else{
+                $pdoStatement = $pdo->prepare('SELECT pseudo from utilisateur;');
+                $pdoStatement->execute();
+                $pseudobon = $pdoStatement->fetch();
+
+                foreach($pseudobon as $ps)
+                {
+                    if($_GET['pseudo'] == $ps)
+                    {
+                        $error_message_pseudo = 'le pseudo doit etre plus long que 4 carctère';
+                    }
+                }
+            }
+        }
+
+        $passwordtest = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$/'; 
+        if(isset($_GET['passe']))
+        {
+            if (!preg_match($passwordtest,$_GET['passe']))
+            {
+                $error_message_passe = 'le mot de passe n est pas valide';
+            }
+        }
+
+        if(isset($_GET['confirm_password']))
+        {
+            if ($_GET['confirm_password'] != $_GET['passe'] )
+            {
+                $error_message_passeconfirm = 'le mot de passe doit etre identique';;
+            }
+        }
+
+        if(
+            !isset($error_message_mail) && 
+            !isset($error_message_pseudo) && 
+            !isset($error_message_passe) &&
+            !isset($error_message_passeconfirm)
+        ){
+            $pdoStatement = $pdo->prepare('INSERT INTO utilisateur (email,mot_de_passe,pseudo) VALUES(:email,:mot_de_passe,:pseudo)');
+            $pdoStatement->execute([
+                ':email' => $_GET['email'],
+                ':mot_de_passe' => hash('sha512', $_GET['confirm_password']),
+                ':pseudo' => $_GET['pseudo'],
+            ]);
+        }
+
+
+
+
+    }
+?>
+    <form method="get">
         <input type="text" name="email" id="email" placeholder=Email style="width: 500px; height: 40px; background-color: #272645 ;
         border-style: none; border-radius: 3px;">
         <br>
@@ -31,7 +101,43 @@ require_once SITE_ROOT . 'utils/database.php';
         border-style: none; border-radius: 3px;">
         <br>
         <br>
-        <button onclick="window.location.href = 'index.html'" class="bouton_connexion">Connexion</button>
+       <div class="erreur_php"> <p><?php 
+                if(isset($error_message_mail))
+                {
+                    echo $error_message_mail;
+                }
+
+                if(isset($error_message_pseudo) )
+                {
+                    echo $error_message_pseudo;
+                }
+
+                if(isset($error_message_passe) )
+                {
+                    echo $error_message_passe;
+                }
+
+                if(isset($error_message_passeconfirm))
+                {
+                    echo $error_message_passeconfirm;
+                }
+                if(
+                    !isset($error_message_mail) && 
+                    !isset($error_message_pseudo) && 
+                    !isset($error_message_passe) &&
+                    !isset($error_message_passeconfirm)
+                ){
+                    echo 'Le compte a bien été créé';
+                }
+
+       
+            ?></p>
+        </div>
+        <button class="bouton_connexion">Connexion</button>
+
+         
+
+
 </form>
 </div>
 
