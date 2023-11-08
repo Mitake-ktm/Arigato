1c37d44d19f5409dd8294fec96dbaf47a034b320
Kilian Martin <kilian.martin@edu.esiee-it.fr>
2023-11-08 11:30:24 +0100

    header lien

--- myAccount.php
+++ myAccount.php
@@ -6,46 +6,43 @@ require_once SITE_ROOT . 'utils/database.php';
 <!DOCTYPE html>
 <html lang="fr">
 <?php require_once SITE_ROOT . 'partials/head.php'; ?>
+
 <body>
-<?php require_once SITE_ROOT . 'partials/header.php'; ?>
+    <?php require_once SITE_ROOT . 'partials/header.php'; ?>
 
     <main>
         <div class="titre_connexion">
             <h1>Mon Compte</h1>
-        </div>        
+        </div>
         <div class="Formulaire_de_connexion">
-        <?php
-        if (!empty($_POST)) {
+            <?php
+            if (!empty($_POST)) {
 
-            if (isset($_POST['Nemail'])) {
-                
-                if (!filter_var($_POST['Nemail'], FILTER_VALIDATE_EMAIL)) 
-                {
-                    $error_message_nouveau_mail = "Le format de l'email n'est pas valide";
-                }
-                
-            }
-    
-            if (isset($_POST['Nemail']) && isset($_POST['email'])){
-    
-                if ($_POST['Nemail'] == $_POST['email'] ){
-    
-                    $error_message_ancien_mail = "nouveau email identique à l'ancien";
-                }
-                
-                $pdoStatement = $pdo->prepare('SELECT email from utilisateur;');
-                $pdoStatement->execute();
-                $emailbon = $pdoStatement->fetchAll();
-    
-                foreach($emailbon as $ea)
-                {
-                    if($_POST['Nemail'] == $ea)
-                    {
-                        $error_message_nouveau_mail = 'le mail existe deja';
+                if (isset($_POST['Nemail'])) {
+
+                    if (!filter_var($_POST['Nemail'], FILTER_VALIDATE_EMAIL)) {
+                        $error_message_nouveau_mail = "Le format de l'email n'est pas valide";
                     }
                 }
-            }
-            /*if(isset($_GET['passe']))
+
+                if (isset($_POST['Nemail']) && isset($_POST['email'])) {
+
+                    if ($_POST['Nemail'] == $_POST['email']) {
+
+                        $error_message_ancien_mail = "nouveau email identique à l'ancien";
+                    }
+
+                    $pdoStatement = $pdo->prepare('SELECT email from utilisateur;');
+                    $pdoStatement->execute();
+                    $emailbon = $pdoStatement->fetchAll();
+
+                    foreach ($emailbon as $ea) {
+                        if ($_POST['Nemail'] == $ea) {
+                            $error_message_nouveau_mail = 'le mail existe deja';
+                        }
+                    }
+                }
+                /*if(isset($_GET['passe']))
             {
                 $passwordtest = '/^(?=.[a-z])(?=.[A-Z])(?=.[0-9])(?=.[!@#$%^&*]).{8,}$/';
                 if (!preg_match($passwordtest,$_GET['passe']))
@@ -53,27 +50,26 @@ require_once SITE_ROOT . 'utils/database.php';
                     $error_message_motdepasse = 'le mot de passe n est pas valide';
                 }*/
             }
-            if(
-                !isset($error_message_ancien_mail) && 
-                !isset($error_message_nouveau_mail) && 
-                !isset($error_message_motdepasse)&&
-                isset($_POST['email'])&&
-                isset($_POST['Nemail'])&&
-                isset($_POST['passe'])) 
-                 
-            {
+            if (
+                !isset($error_message_ancien_mail) &&
+                !isset($error_message_nouveau_mail) &&
+                !isset($error_message_motdepasse) &&
+                isset($_POST['email']) &&
+                isset($_POST['Nemail']) &&
+                isset($_POST['passe'])
+            ) {
                 $pdoStatement = $pdo->prepare('UPDATE utilisateur SET email=:Nemail WHERE email=:email AND mot_de_passe=:passe');
                 $pdoStatement->execute([
                     ':email' => $_POST['email'],
                     ':Nemail' => $_POST['Nemail'],
                     ':passe' => hash('sha512', $_POST['passe']),
-                    
+
                 ]);
             }
-        
-    
-    
-            
+
+
+
+
             ?>
 
             <form method="POST">
@@ -92,52 +88,48 @@ require_once SITE_ROOT . 'utils/database.php';
                 border-style: none; border-radius: 3px;">
                 <br>
                 <br>
-                <div class="erreur_php"> <p><?php 
-        if(isset($error_message_ancien_mail))
-                {
-                    echo $error_message_ancien_mail;
-                }
+                <div class="erreur_php">
+                    <p><?php
+                        if (isset($error_message_ancien_mail)) {
+                            echo $error_message_ancien_mail;
+                        }
 
-                if(isset($error_message_nouveau_mail))
-                {
-                    echo $error_message_nouveau_mail;
-                }
+                        if (isset($error_message_nouveau_mail)) {
+                            echo $error_message_nouveau_mail;
+                        }
 
-                if(isset($error_message_motdepasse))
-                {
-                    echo $error_message_motdepasse;
-                }
-                if(
-                    !isset($error_message_ancien_mail) && 
-                    !isset($error_message_nouveau_mail) && 
-                    !isset($error_message_motdepasse)&&
-                    isset($_POST['email'])&&
-                    isset($_POST['Nemail'])&&
-                    isset($_POST['passe']))   
-                    {
-                    echo "L'email a bien été modifié";
-                }
-                ?></p>
+                        if (isset($error_message_motdepasse)) {
+                            echo $error_message_motdepasse;
+                        }
+                        if (
+                            !isset($error_message_ancien_mail) &&
+                            !isset($error_message_nouveau_mail) &&
+                            !isset($error_message_motdepasse) &&
+                            isset($_POST['email']) &&
+                            isset($_POST['Nemail']) &&
+                            isset($_POST['passe'])
+                        ) {
+                            echo "L'email a bien été modifié";
+                        }
+                        ?></p>
                 </div>
                 <button class="bouton_connexion">Changer l'email</button>
-        </form>
+            </form>
         </div>
 
-                
-        <div class="Formulaire_de_connexion">
-        <?php
-if (!empty($_POST)) {
-    if (isset($_POST['Npasse']) && isset($_POST['passe'])){
-    
-        if ($_POST['Npasse'] == $_POST['passe'] ){
 
-            $error_message_ancien_motdepasse = "nouveau mot de passe identique à l'ancien";
-        }
-    }
-        
-       
-    }
-    /*if(isset($_GET['Npasse']))
+        <div class="Formulaire_de_connexion">
+            <?php
+            if (!empty($_POST)) {
+                if (isset($_POST['Npasse']) && isset($_POST['passe'])) {
+
+                    if ($_POST['Npasse'] == $_POST['passe']) {
+
+                        $error_message_ancien_motdepasse = "nouveau mot de passe identique à l'ancien";
+                    }
+                }
+            }
+            /*if(isset($_GET['Npasse']))
     {
         $passwordtest = '/^(?=.[a-z])(?=.[A-Z])(?=.[0-9])(?=.[!@#$%^&*]).{8,}$/';
         if (!preg_match($passwordtest,$_GET['Npasse']))
@@ -145,31 +137,27 @@ if (!empty($_POST)) {
             $error_message_nouveau_motdepasse = 'le mot de passe n est pas valide';
         }
     }*/
-    if(isset($_POST['Cpasse']))
-        {
-            if ($_POST['Cpasse'] != $_POST['Npasse'] )
-            {
-                $error_message_passeconfirm = 'le mot de passe doit etre identique';;
+            if (isset($_POST['Cpasse'])) {
+                if ($_POST['Cpasse'] != $_POST['Npasse']) {
+                    $error_message_passeconfirm = 'le mot de passe doit etre identique';;
+                }
             }
-        }
-    if(
-        !isset($error_message_ancien_motdepasse) && 
-        !isset($error_message_nouveau_motdepasse) && 
-        !isset($error_message_passeconfirm)&&
-        isset($_POST['passe'])&&
-        isset($_POST['Npasse'])&&
-        isset($_POST['Cpasse'])) 
-         
-    {
-        $pdoStatement = $pdo->prepare('UPDATE utilisateur SET mot_de_passe=:Cpasse WHERE mot_de_passe=:passe');
-        $pdoStatement->execute([
-            ':passe' => hash('sha512', $_POST['passe']),
-            ':Cpasse' => hash('sha512', $_POST['Cpasse']),
-            
-        ]);
+            if (
+                !isset($error_message_ancien_motdepasse) &&
+                !isset($error_message_nouveau_motdepasse) &&
+                !isset($error_message_passeconfirm) &&
+                isset($_POST['passe']) &&
+                isset($_POST['Npasse']) &&
+                isset($_POST['Cpasse'])
+            ) {
+                $pdoStatement = $pdo->prepare('UPDATE utilisateur SET mot_de_passe=:Cpasse WHERE mot_de_passe=:passe');
+                $pdoStatement->execute([
+                    ':passe' => hash('sha512', $_POST['passe']),
+                    ':Cpasse' => hash('sha512', $_POST['Cpasse']),
 
-}
-?>
+                ]);
+            }
+            ?>
             <form method="POST">
                 <label for="passe">Changer votre mot de passe :</label>
                 <br>
@@ -185,37 +173,36 @@ if (!empty($_POST)) {
                 <input type="password" name="Cpasse" id="passe" placeholder="Comfirmer Mot de passe" style="width: 500px; height: 40px; background-color: #272645 ;
                 border-style: none; border-radius: 3px;">
                 <br>
-                <div class="erreur_php"> <p><?php 
-                if(isset($error_message_ancien_motdepasse))
-                {
-                    echo $error_message_ancien_motdepasse;
-                }
+                <div class="erreur_php">
+                    <p><?php
+                        if (isset($error_message_ancien_motdepasse)) {
+                            echo $error_message_ancien_motdepasse;
+                        }
 
-                if(isset($error_message_nouveau_motdepasse))
-                {
-                    echo $error_message_nouveau_motdepasse;
-                }
+                        if (isset($error_message_nouveau_motdepasse)) {
+                            echo $error_message_nouveau_motdepasse;
+                        }
 
-                if(isset($error_message_passeconfirm))
-                {
-                    echo $error_message_passeconfirm;
-                }
-                if(
-                    !isset($error_message_ancien_motdepasse) && 
-                    !isset($error_message_nouveau_motdepasse) && 
-                    !isset($error_message_passeconfirm)&&
-                    isset($_POST['passe'])&&
-                    isset($_POST['Npasse'])&&
-                    isset($_POST['Cpasse']))   
-                    {
-                    echo "Le mot de passe a bien été modifié";
-                }
-                ?></p>
+                        if (isset($error_message_passeconfirm)) {
+                            echo $error_message_passeconfirm;
+                        }
+                        if (
+                            !isset($error_message_ancien_motdepasse) &&
+                            !isset($error_message_nouveau_motdepasse) &&
+                            !isset($error_message_passeconfirm) &&
+                            isset($_POST['passe']) &&
+                            isset($_POST['Npasse']) &&
+                            isset($_POST['Cpasse'])
+                        ) {
+                            echo "Le mot de passe a bien été modifié";
+                        }
+                        ?></p>
                 </div>
                 <button class="bouton_connexion">Changer le mot de passe</button>
-        </form>
+            </form>
         </div>
     </main>
     <?php require_once SITE_ROOT . 'partials/footer.php'; ?>
 </body>
+
 </html>
--- partials/header.php
+++ partials/header.php
@@ -1,52 +1,62 @@
-     <div class="titre">
+<div class="titre">
+    <!-- PSEUDO JOUEUR CONNECTÉ -->
+    <?php
+    if (isset($_SESSION['userId'])) {
+        $userID = $_SESSION['userId'];
+        $pdoStatement = $pdo->prepare('SELECT * From utilisateur WHERE id = :id ');
+        $pdoStatement->execute([
+            ':id' => $userID,
+        ]);
+        $utilisateur = $pdoStatement->fetch();
 
-         <!-- PSEUDO JOUEUR CONNECTÉ -->
+        echo "bonjour " . $utilisateur->pseudo . " !";
+    } else {
+        echo "pas de joueur connecté";
+    }
+    ?>
+    <!-- FIN PSEUDO JOUEUR CONNECTÉ -->
 
-         <?php
-         if(isset($_SESSION['userId']))
-         {
-            $userID = $_SESSION['userId']; 
-            $pdoStatement = $pdo->prepare('SELECT * From utilisateur WHERE id = :id ');
-            $pdoStatement->execute([
-                ':id' => $userID,
-            ]);
-            $utilisateur = $pdoStatement->fetch();
-            
-            echo "bonjour " . $utilisateur->pseudo . " !";
-        }
-            /*else echo "pas de joueur connecté";*/ ?>
+    <!-- Menu -->
+    <p><b>Shinkei Suijaku</b></p>
+    <nav>
+        <ul class="menu">
+            <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'index.php') ? 'class="active"' : ''; ?>>
+                <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>index.php"><b>ACCUEIL</b></a>
+            </li>
+            <?php if (isset($_SESSION['userId'])) : ?>
+                <!-- Condition pour afficher "JEU" uniquement lorsque l'utilisateur est connecté -->
+                <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'games/memory/index.php') ? 'class="active"' : ''; ?>>
+                    <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>games/memory/index.php"><b>JEU</b></a>
+                </li>
+            <?php endif; ?>
+            <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'games/memory/scores.php') ? 'class="active"' : ''; ?>>
+                <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>games/memory/scores.php"><b>SCORES</b></a>
+            </li>
+            <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'contact.php') ? 'class="active"' : ''; ?>>
+                <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>contact.php"><b>NOUS CONTACTER</b></a>
+            </li>
+            <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'login.php') ? 'class="active"' : ''; ?>>
+                <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>login.php"><b>CONNEXION</b></a>
+            </li>
+            <?php
+            if (isset($_SESSION['userId'])) {
+            ?>
+                <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'myAccount.php') ? 'class="active"' : ''; ?>>
+                    <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>myAccount.php"><b>MON COMPTE</b></a>
+                </li>
 
-
-         <!-- FIN PSEUDO JOUEUR CONNECTÉ -->
-
-
-
-
-
-
-         <!-- Menu -->
-
-         <p><b>Shinkei Suijaku</b></p>
-         <nav>
-             <ul class="menu">
-
-                 <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'index.php') ? 'class="active"' : ''; ?>>
-                     <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>index.php"><b>ACCUEIL</b></a>
-                 </li>
-
-                 <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'games/memory/index.php') ? 'class="active"' : ''; ?>>
-                     <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>games/memory/index.php"><b>JEU</b></a>
-                 </li>
-
-                 <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'games/memory/scores.php') ? 'class="active"' : ''; ?>>
-                     <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>games/memory/scores.php"><b>SCORES</b></a>
-                 </li>
-
-                 <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'contact.php') ? 'class="active"' : ''; ?>>
-                     <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>contact.php"><b>NOUS CONTACTER</b></a>
-                 </li>
-             </ul>
-         </nav>
-     </div>
-
-     </header>
+                <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'disconnect.php') ? 'class="active"' : ''; ?>>
+                    <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>disconnect.php"><b>Sayonara ! </b></a>
+                </li>
+            <?php
+            } else {
+            ?>
+                <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'register.php') ? 'class="active"' : ''; ?>>
+                    <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>register.php"><b>INSCRIPTION</b></a>
+                </li>
+            <?php
+            }
+            ?>
+        </ul>
+    </nav>
+</div>
