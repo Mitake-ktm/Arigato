3a918cdc921adf835f22a765f6f8575216f5a18b
Antony Dias <antony.overwatch@gmail.com>
2023-11-09 09:01:30 +0100

    avancé

--- chat.php
+++ chat.php
@@ -1,44 +1,33 @@
-<div class="tchat">
-            <div class="tchat_head">
+<<div class="tchat">
+<div class="tchat_head">
                 <div class="connexion_logo">
-                    <img src="assets/images/anto.jpg" alt="pp_anto">
-                </div>
-                <div class="rondvert">
-                    <img src="assets/images/rond.png" alt="rond de connexion">
-
+                    <img src="<?= PROJECT_FOLDER ?>assets/images/anto.jpg" alt="pp_anto">
                 </div>
                 <div class="texte_head_chat">
                     <p>Chat Global</p>
-
                 </div>
             </div>
 
             <div class="tchat_body">
-
                 <div class="gauche">
-                <div class="bulle_gauche">
-                    <div class="fleche_gauche"></div>
-                    Hello World !
-                </div>
-                <p>moi</p>
+                <div id="messages" class="bulle_gauche">
+
+            </div>
             </div>
 
                 <div class="droite">
                 <div class="bulle_droite">
-                    <div class="fleche_droite"></div>
                     Hello !!
                 </div>
-                <p>pseudo</p>
             </div>
 
                 </div>
                 <div class="chat_bas">
-                    <form action="chat">
-                        <input type="text" name="chat" id="chat" placeholder=| style="width: 200px; height: 40px; background-color: #1E90FF;
-        border-style: none; border-radius: 3px;">
+                    <form method="post">
+                        <input type="text" name="message" id="chat"  style="width: 200px; height: 40px; background-color: #1E90FF;
+                        border-style: none; border-radius: 3px;">
+                        <input onclick="ajaxEnvoie()" type="submit" name="valider" >
                     </form>
-
-                    <img src="assets/images/bouton envoie.png" alt="bouton envoie">
-
                 </div>
-            </div>
+
+</div>
--- partials/header.php
+++ partials/header.php
@@ -20,27 +20,24 @@
     <p><b>Shinkei Suijaku</b></p>
     <nav>
         <ul class="menu">
-            <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'index.php') ? 'class="active"' : ''; ?>>
-                <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>index.php"><b>ACCUEIL</b></a>
-            </li>
-            <?php if (isset($_SESSION['userId'])) : ?>
-                <!-- Condition pour afficher "JEU" uniquement lorsque l'utilisateur est connecté -->
-                <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'games/memory/index.php') ? 'class="active"' : ''; ?>>
-                    <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>games/memory/index.php"><b>JEU</b></a>
-                </li>
-            <?php endif; ?>
-            <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'games/memory/scores.php') ? 'class="active"' : ''; ?>>
-                <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>games/memory/scores.php"><b>SCORES</b></a>
-            </li>
-            <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'contact.php') ? 'class="active"' : ''; ?>>
-                <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>contact.php"><b>NOUS CONTACTER</b></a>
-            </li>
-            <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'login.php') ? 'class="active"' : ''; ?>>
-                <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>login.php"><b>CONNEXION</b></a>
-            </li>
             <?php
             if (isset($_SESSION['userId'])) {
             ?>
+                <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'index.php') ? 'class="active"' : ''; ?>>
+                    <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>index.php"><b>ACCUEIL</b></a>
+                </li>
+                <?php if (isset($_SESSION['userId'])) : ?>
+                    <!-- Condition pour afficher "JEU" uniquement lorsque l'utilisateur est connecté -->
+                    <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'games/memory/index.php') ? 'class="active"' : ''; ?>>
+                        <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>games/memory/index.php"><b>JEU</b></a>
+                    </li>
+                <?php endif; ?>
+                <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'games/memory/scores.php') ? 'class="active"' : ''; ?>>
+                    <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>games/memory/scores.php"><b>SCORES</b></a>
+                </li>
+                <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'contact.php') ? 'class="active"' : ''; ?>>
+                    <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>contact.php"><b>NOUS CONTACTER</b></a>
+                </li>
                 <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'myAccount.php') ? 'class="active"' : ''; ?>>
                     <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>myAccount.php"><b>MON COMPTE</b></a>
                 </li>
@@ -51,8 +48,27 @@
             <?php
             } else {
             ?>
+                <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'index.php') ? 'class="active"' : ''; ?>>
+                    <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>index.php"><b>ACCUEIL</b></a>
+                </li>
+                <?php if (isset($_SESSION['userId'])) : ?>
+                    <!-- Condition pour afficher "JEU" uniquement lorsque l'utilisateur est connecté -->
+                    <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'games/memory/index.php') ? 'class="active"' : ''; ?>>
+                        <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>games/memory/index.php"><b>JEU</b></a>
+                    </li>
+                <?php endif; ?>
+                <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'games/memory/scores.php') ? 'class="active"' : ''; ?>>
+                    <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>games/memory/scores.php"><b>SCORES</b></a>
+                </li>
+                <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'contact.php') ? 'class="active"' : ''; ?>>
+                    <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>contact.php"><b>NOUS CONTACTER</b></a>
+
                 <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'register.php') ? 'class="active"' : ''; ?>>
                     <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>register.php"><b>INSCRIPTION</b></a>
+                </li>
+
+                <li <?php echo ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'login.php') ? 'class="active"' : ''; ?>>
+                    <a style="text-decoration: none;" href="<?= PROJECT_FOLDER ?>login.php"><b>CONNEXION</b></a>
                 </li>
             <?php
             }
@@ -61,4 +77,4 @@
     </nav>
 </div>
 
-     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
